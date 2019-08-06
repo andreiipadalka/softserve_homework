5bc89298fec9ede14a6bb94c5ef56234d34be855
Andreii <an9reii@gmail.com>
2019-08-06 11:50:59 +0300

    Added first working version of GameStore

---
+++ demo_1/GameStore/GameStore.Domain/Abstract/Interface1.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using GameStore.Domain.Entities;
+
+namespace GameStore.Domain.Abstract
+{
+    public interface IGameRepository
+    {
+        IEnumerable<Game> Games { get; }
+    }
+}
---
+++ demo_1/GameStore/GameStore/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+
+namespace GameStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { Name = "SimCity", Price = 1 },
+                new Game { Name = "Heroes", Price=100000500000 },
+                new Game { Name = "Morrowind", Price=100000050000 },
+                new Game { Name = "TITANFALL", Price=1 },
+                new Game { Name = "Lol", Price=77 },
+                new Game { Name = "olololo", Price=2299 },
+                new Game { Name = "Battlefield V", Price=1 }
+            });
+            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
+        }
+    }
+}
